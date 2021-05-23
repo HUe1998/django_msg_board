2e087836535eb06ad79ded4c963ba4d4c0914046
HUe1998 <wasddsawwasddsawwasd@gmail.com>
2021-05-23 18:14:47 +0530

    configure heroku

--- mesg_board/settings.py
+++ mesg_board/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c@0ht$ry*b*a&-ddgbg0z^eu3jh=it#ww))w(hjt@#(1=ntvy9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
